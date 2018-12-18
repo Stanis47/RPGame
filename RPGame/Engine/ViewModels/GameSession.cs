5ec986913d4fb91d28277e9067b01acd6b4a52bf
Stanis <Stanis@DESKTOP-K7VC437>
2018-12-18 23:26:44 +0200

    Added GameSession view model

---
+++ RPGame/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,19 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Stanis";
+            CurrentPlayer.Gold = 100;
+        }
+    }
+}
