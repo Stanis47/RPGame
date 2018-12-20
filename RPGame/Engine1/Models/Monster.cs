1899b6b98545ed54a07efcb2ea74e7ce5592b75d
Stanis <Stanis@DESKTOP-K7VC437>
2018-12-20 13:38:50 +0200

    Created monster factory

---
+++ RPGame/Engine1/Models/Monster.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Monster : BaseNotificationClass
+    {
+        private int _hitPoints;
+
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public int MaximumHitPoints { get; private set; }
+        public int HitPoints
+        {
+            get => _hitPoints;
+            set
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+
+        public int RewardExperiencePoints { get; private set; }
+        public int RewardGold { get; private set; }
+
+        public ObservableCollection<ItemQuantity> Inventory { get; set; }
+
+        public Monster(string name, string imageName,
+            int maxHitPoints, int hitPoints,
+            int rewardExperiencePoints, int rewardGold)
+        {
+            this.Name = name;
+            this.ImageName = String.Format("/Engine1;component/Images/Monsters/{0}", imageName);
+            this.MaximumHitPoints = maxHitPoints;
+            this.HitPoints = hitPoints;
+            this.RewardExperiencePoints = rewardExperiencePoints;
+            this.RewardGold = rewardGold;
+
+            this.Inventory = new ObservableCollection<ItemQuantity>();
+        }
+    }
+}
