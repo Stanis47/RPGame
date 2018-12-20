930f4de8d96599ed895b34748780231e341c6393
Stanis <Stanis@DESKTOP-K7VC437>
2018-12-20 12:05:33 +0200

    Created quests factory & ItemQuantity class

---
+++ RPGame/Engine1/Models/Quest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Quest
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<ItemQuantity> ItemsToComplete { get; set; }
+
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+        public List<ItemQuantity> RewardItems { get; set; }
+
+        public Quest(int id, string name, string description, List<ItemQuantity> itemsToComplete,
+            int rewardEXP, int rewardGold, List<ItemQuantity> rewardItems)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Description = description;
+            this.ItemsToComplete = itemsToComplete;
+            this.RewardExperiencePoints = rewardEXP;
+            this.RewardGold = rewardGold;
+            this.RewardItems = rewardItems;
+        }
+    }
+}
